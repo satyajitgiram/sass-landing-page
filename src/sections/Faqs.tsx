488512bd1a19e62087dafc4125255db724d52289
Satyajit Giram <satyajitgiram2000@gmail.com>
2024-11-23 20:26:13 +0530

    added FAQs section

--- src/sections/Faqs.tsx
+++ src/sections/Faqs.tsx
@@ -1,3 +1,6 @@
+import Tag from "@/components/Tag";
+import { twMerge } from "tailwind-merge";
+
 const faqs = [
     {
         question: "How is Layers different from other design tools?",
@@ -22,14 +25,50 @@ const faqs = [
 ];
 
 export default function Faqs() {
+    const selectedIndex = 0;
     return (
-        <>
-            {faqs.map((faq: { question: string; answer: string }, index: number) => (
-                <div key={index} className="mb-8">
-                    <h3 className="text-lg font-bold mb-2">{faq.question}</h3>
-                    <p>{faq.answer}</p>
+        <section className="py-24">
+            <div className="container">
+                <div className="flex justify-center">
+                    <Tag className="text-lime-400">FAQs</Tag>
                 </div>
-            ))}
-        </>
+
+                <h2 className="text-6xl font-medium mt-6 text-center max-w-xl mx-auto">
+                    Questions? We&apos;ve got{" "}
+                    <span className="text-lime-400">answers</span>
+                </h2>
+                <div className="mt-12 flex flex-col gap-6 max-w-xl mx-auto">
+                    {faqs.map((faq, faqIndex) => (
+                        <div key={faqIndex} className="bg-neutral-900 border border-white/10 p-6 rounded-2xl">
+                            <div className="flex justify-between items-center">
+                                <h3 className="font-medium">
+                                    {faq.question}
+                                </h3>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    className={twMerge("feather feather-plus text-lime-400 flex-shrink-0", selectedIndex === faqIndex && "rotate-45")}
+                                >
+                                    <line x1="12" y1="5" x2="12" y2="19"></line>
+                                    <line x1="5" y1="12" x2="19" y2="12"></line>
+                                </svg>
+                            </div>
+                            <div className={twMerge("mt-6", selectedIndex !== faqIndex && "hidden")}>
+                                <p className="text-white/50">
+                                    {faq.answer}
+                                </p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
     );
 }
