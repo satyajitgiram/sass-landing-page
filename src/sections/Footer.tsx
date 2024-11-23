95276ee2265fed9dfa608e5cf09c6da74a79196e
Satyajit Giram <satyajitgiram2000@gmail.com>
2024-11-23 21:05:54 +0530

    Added footer section

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,7 @@
+import logoImage from '@/assets/images/logo.svg';
+import Image from 'next/image';
+
+
 const footerLinks = [
     { href: "#", label: "Contact" },
     { href: "#", label: "Privacy Policy" },
@@ -6,12 +10,23 @@ const footerLinks = [
 
 export default function Footer() {
     return (
-        <>
-            {footerLinks.map((link: { href: string; label: string }, index: number) => (
-                <a key={index} href={link.href} className="mr-4">
-                    {link.label}
-                </a>
-            ))}
-        </>
+        <section className='py-16'>
+            <div className="container">
+                <div className='flex flex-col md:flex-row items-center md:justify-between gap-6'>
+                    <div>
+                        <Image src={logoImage} alt="Layers logo" />
+                    </div>
+                    <div>
+                        <nav className='flex gap-6'>
+                            {footerLinks.map((link, index) => (
+                                <a key={index} href={link.href} className="text-white/50 text-sm">
+                                    {link.label}
+                                </a>
+                            ))}
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
