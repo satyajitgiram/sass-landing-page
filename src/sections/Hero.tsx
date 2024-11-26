6e3f6f663515dde333fd08330335a01bc06f811c
Satyajit Giram <satyajitgiram2000@gmail.com>
2024-11-26 06:43:48 +0530

    Added framer motion Animations, draggable, custom cursor

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,27 +1,58 @@
+'use client';
 import Button from "@/components/Button";
 import DesignExample1 from '@/assets/images/design-example-1.png';
 import DesignExample2 from '@/assets/images/design-example-2.png';
 import Image from "next/image";
 import Pointer from "@/components/Pointer";
+import {motion, useAnimate} from 'framer-motion';
+import { useEffect } from "react";
+import cursorYouImage from '@/assets/images/cursor-you.svg';
 
 export default function Hero() {
+    const [leftDesignScope, leftDesignAnimate]= useAnimate();
+    const [leftPointerScope, leftPointerAnimate]= useAnimate();
+    const [rightDesignScope, rightDesignAnimate]= useAnimate();
+    const [rightPointerScope, rightPointerAnimate]= useAnimate();
+
+    useEffect(() => {
+        leftDesignAnimate([
+            [leftDesignScope.current,{opacity:[0,1]},{duration:0.5}],
+            [leftDesignScope.current,{y:0, x:0},{duration:0.5}] 
+        ])
+        leftPointerAnimate([
+            [leftPointerScope.current,{opacity:1},{duration:0.5}],
+            [leftPointerScope.current,{y:0, x:-100},{duration:0.5}],
+            [leftPointerScope.current,{x: 0, y:[0,16,0]},{duration:0.5, ease:'easeInOut'}]
+        ])
+        rightDesignAnimate([
+            [rightDesignScope.current,{opacity:1},{duration:0.5, delay:1.5}],
+            [rightDesignScope.current,{x:0, y:0},{duration:0.5}]
+        ])
+        rightPointerAnimate([
+            [rightPointerScope.current,{opacity:1},{duration:0.5, delay:1.5}],
+            [rightPointerScope.current,{x:175, y:0},{duration:0.5}],
+            [rightPointerScope.current,{x: 0, y:[0,20,0] },{duration:0.5, ease:'easeInOut'}]
+        ]) 
+    },[])
+
     return (
-        <section className="py-24 overflow-x-clip">
+        <section className="py-24 overflow-x-clip" style={{
+            cursor: `url(${cursorYouImage.src}), auto`,
+        }}>  
             <div className="container relative">
-                <div className="absolute -left-32 top-16 hidden lg:block">
-                    <Image src={DesignExample1} alt="Design Example1 Image" />
-                </div>
-
-                <div className="absolute -right-64 -top-16  hidden lg:block ">
-                    <Image src={DesignExample2} alt="Design Example2 Image" />
-                </div>
-
-                <div className="absolute left-56 top-96  hidden lg:block">
+                <motion.div ref={leftDesignScope} initial={{opacity:0, y:100, x:-100}} drag className="absolute -left-32 top-16 hidden lg:block">
+                    <Image src={DesignExample1} draggable="false" alt="Design Example1 Image" />
+                </motion.div>
+                <motion.div ref={leftPointerScope} initial={{opacity:0, y:100, x:-200}} className="absolute left-56 top-96  hidden lg:block">
                     <Pointer name="Andrea" />
-                </div>
-                <div className="absolute right-80 -top-4  hidden lg:block">
+                </motion.div> 
+
+                <motion.div ref={rightDesignScope} initial={{opacity: 0, x:100, y:100}} drag className="absolute -right-64 -top-16  hidden lg:block ">
+                    <Image src={DesignExample2} draggable="false" alt="Design Example2 Image" />
+                </motion.div>
+                <motion.div ref={rightPointerScope} initial={{opacity:0, x:275, y:100}} className="absolute right-80 -top-4  hidden lg:block">
                     <Pointer name="John" color="red" />
-                </div>
+                </motion.div>
 
                 <div className="flex justify-center">
                     <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-900 font-semibold">
