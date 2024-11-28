d62250c113283124997962e256b533c92828666e
Satyajit Giram <satyajitgiram2000@gmail.com>
2024-11-28 18:13:02 +0530

    fixed top in large screen

--- src/sections/Introduction.tsx
+++ src/sections/Introduction.tsx
@@ -25,7 +25,7 @@ export default function Introduction() {
     return (
         <section className="py-28 lg:py-40">
             <div className="container">
-                <div className="sticky top-20 md:top:28 lg:top-40">
+                <div className="sticky top-20 md:top:28 lg:top-32">
                     <div className="flex justify-center">
                     <Tag>Introducing Layers</Tag>
                     </div>
